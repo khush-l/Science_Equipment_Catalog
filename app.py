569c17c5597f8da6d85225bf79351a5c817a4b6c
khushl <khush@shubhsoln.com>
2024-04-30 11:02:28 -0500

    Creating basic file structure

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+import flask as fk
+import logging
+import sqlite3
+import time
+
+app = fk.Flask(__name__, '/static')
+connection = sqlite3.connect("total.db")
+cursor= connection.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, date_t TEXT, title TEXT, art TEXT)")
+
+@app.route('/', methods=["GET", "POST"])
+def root():
+    method = fk.request.method
+    if method == "GET":
+        logging.info("********** root GET **********")
+        return fk.render_template(
+            'home.html',
+            arts=[],
+            error=""
+        )
+#@app.route('/check', methods=["GET", "POST"])
+
